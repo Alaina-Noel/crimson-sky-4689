0340b58f599319b3af00282bf4d343196f0c197e
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-18 09:22:30 -0600

    Tests made for model

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'dishes show page', type: :feature do
     describe 'When I visit the dishes show page' do
         before :each do
           @alaina = Chef.create!(name: "Alaina")
+          @mallory = Chef.create!(name: "Mallory")
 
           @basil_soup = @alaina.dishes.create!(name: "Tomato Basil Soup", description: "Delicious & Creamy")
           @chicken_feta = @alaina.dishes.create!(name: "Chicken Feta", description: "Chicken with Yogurt Feta Sauce")
@@ -32,33 +33,38 @@ RSpec.describe 'dishes show page', type: :feature do
           @perfectsalmon_salmon = DishIngredient.create!(dish_id: @perfect_salmon.id , ingredient_id: @salmon.id)
           @perfectsalmon_salt = DishIngredient.create!(dish_id: @perfect_salmon.id , ingredient_id: @salt.id)
 
-#salt is used 4 times
-#garlic is used 3 times
-# tomato is used 2 times
-#salmon used one time
-#yogurt used not at all
-
+          
           visit "/dishes/#{@basil_soup.id}"
         end
+        
+        it 'I see the dishs name and description, a list of ingredients for that dish, and I see the chefs name.' do
+          
+          expect(page).to have_content("Dish Name: #{@basil_soup.name}")
+          expect(page).to have_content("Description: #{@basil_soup.description}")
 
-      it 'I see the dishs name and description, a list of ingredients for that dish, and I see the chefs name.' do
+          within('#ingredients_list') do
+            expect(page).to have_content("salt")
+            expect(page).to have_content("garlic")
+            expect(page).to have_content("tomato")
+            expect(page).to_not have_content("yogurt")
+          end
 
-        expect(page).to have_content("Dish Name: #{@basil_soup.name}")
-        expect(page).to have_content("Description: #{@basil_soup.description}")
-        within('#ingredients_list') do
-          expect(page).to have_content("salt")
-          expect(page).to have_content("garlic")
-          expect(page).to have_content("tomato")
-          expect(page).to_not have_content("yogurt")
+          expect(page).to have_content("Chef's Name: Alaina")
+          expect(page).to_not have_content("Chef's Name: Mallory")
+
         end
-        expect(page).to have_content("Chef's Name: Alaina")
+        
+        it 'I see the total calorie count for that dish.' do
+          expect(page).to have_content("Total Calorie Count: #{@basil_soup.total_calorie_count}")
+        end
+        
       end
-
-      xit 'I see the total calorie count for that dish.' do
-        expect(page).to have_content("Total Calorie Count: #{@basil_soup.total_calorie_count}")
-      end
-
     end
   end
-end
-
+  
+  
+  #salt is used 4 times
+  #garlic is used 3 times
+  # tomato is used 2 times
+  #salmon used one time
+  #yogurt used not at all
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -11,5 +11,46 @@ RSpec.describe Dish, type: :model do
     it {should have_many(:dish_ingredients)}
   end
 
+  before(:each) do
+       @alaina = Chef.create!(name: "Alaina")
+        @mallory = Chef.create!(name: "Mallory")
+
+        @basil_soup = @alaina.dishes.create!(name: "Tomato Basil Soup", description: "Delicious & Creamy")
+        @chicken_feta = @alaina.dishes.create!(name: "Chicken Feta", description: "Chicken with Yogurt Feta Sauce")
+        @broccoli_bake = @alaina.dishes.create!(name: "Broccoli Bake", description: "Tomato & Broccoli Baked to Perfection")
+        @perfect_salmon = @alaina.dishes.create!(name: "Perfect Grilled Salmon", description: "Science backed perfectly grilled salmon with dill sauce")
+
+        @salt = Ingredient.create!(name: "salt", calories: 100)
+        @chicken = Ingredient.create!(name: "chicken", calories: 200)
+        @garlic = Ingredient.create!(name: "garlic", calories: 20)
+        @yogurt = Ingredient.create!(name: "yogurt", calories: 115)
+        @salmon = Ingredient.create!(name: "salmon", calories: 175)
+        @tomato = Ingredient.create!(name: "tomato", calories: 50)
+
+        @basilsoup_salt = DishIngredient.create!(dish_id: @basil_soup.id , ingredient_id: @salt.id)
+        @basilsoup_tomato = DishIngredient.create!(dish_id: @basil_soup.id , ingredient_id: @tomato.id)
+        @basilsoup_garlic = DishIngredient.create!(dish_id: @basil_soup.id , ingredient_id: @garlic.id)
+
+        @chickenfeta_salt = DishIngredient.create!(dish_id: @chicken_feta.id , ingredient_id: @salt.id)
+        @chickenfeta_garlic = DishIngredient.create!(dish_id: @chicken_feta.id , ingredient_id: @garlic.id)
+
+        @broccolibake_salt = DishIngredient.create!(dish_id: @broccoli_bake.id , ingredient_id: @salt.id)
+        @broccolibake_tomato = DishIngredient.create!(dish_id: @broccoli_bake.id , ingredient_id: @tomato.id)
+        @broccolibake_garlic = DishIngredient.create!(dish_id: @broccoli_bake.id , ingredient_id: @garlic.id)
+
+        @perfectsalmon_salmon = DishIngredient.create!(dish_id: @perfect_salmon.id , ingredient_id: @salmon.id)
+        @perfectsalmon_salt = DishIngredient.create!(dish_id: @perfect_salmon.id , ingredient_id: @salt.id)
+  end
+
+
+  describe 'instance methods' do
+    describe '#total_calorie_count' do
+     it 'counts all calories within a given dish' do
+
+      expect(@basil_soup.total_calorie_count).to eq(170)
+     end
+    end
+  end
 
 end
+
