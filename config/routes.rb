e240ea4e67eddda2c568b3157730e38fdb5b65bf
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-18 09:35:23 -0600

    chefs name on page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
   get '/dishes/:id', to: 'dishes#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/chefs/:id', to: 'chefs#show'
+
 end
 
-
-# get '/mechanics', to: 'mechanics#index'
-# get '/mechanics/:id', to: 'mechanics#show'
 # post '/mechanics/:id/rides/new', to: 'mechanic_rides#create'
 # get '/amusement_parks/:id', to: 'amusement_parks#show'
