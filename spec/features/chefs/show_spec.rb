8f696cc7a71fd012f25152d1509bf280ebc23783
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-18 09:58:51 -0600

    chefs ingredient link on chefs show page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -37,9 +37,8 @@ RSpec.describe 'chefs show page', type: :feature do
         end
 
       it 'I see the name of that chef & a link to view a list of all ingredients that this chef uses in their dishes' do
-
-        expect(page).to have_content("Chef's Name: #{@alaina.name}")
-
+save_and_open_page
+        expect(page).to have_link("#{@alaina.name}'s Ingredients")
       end
 
       xit "When I click on that link I'm taken to a chef's ingredient index page and I can see a unique list of names of all the ingredients that this chef uses" do
