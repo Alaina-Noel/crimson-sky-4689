dd273ec2b0223f1c1c9ee37a04350542470ad8a7
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-18 09:25:37 -0600

    calorie count done

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,8 @@ class Dish < ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def total_calorie_count
+    dish_ingredients.joins(:ingredient).sum(:calories)
+  end
 end
